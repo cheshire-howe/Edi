87982f3537a488243b51e52a62825534973a635f
cheshire-howe <joshzoff@gmail.com>
2015-03-20 12:45:27 -0400

    Added User Admin pages

---
+++ Edi.WebUI/Models/AdminViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Edi.WebUI.Models
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
+    }
+}
